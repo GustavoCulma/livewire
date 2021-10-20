5de4e4c2e57465db7c32b62ab2d3339195e8275b
Gustavo Culma <culmagustavo743@gmail.com>
2021-10-19 20:36:02 -0500

    iconos de font awesome

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -10,65 +10,103 @@
         <x-table>
 
             <div class="px-6 py-4">
-               {{--<input type="text" wire:model="search"> --}} 
-               <x-jet-input class="w-full" placeholder="Escriba lo quiera buscar" type="text" wire:model="search"/>
+                {{-- <input type="text" wire:model="search"> --}}
+                <x-jet-input class="w-full" placeholder="Escriba lo quiera buscar" type="text"
+                    wire:model="search" />
             </div>
             @if ($posts->count())
-            <table class="w-full divide-y divide-gray-200">
+                <table class="w-full divide-y divide-gray-200">
 
-                <thead class="bg-gray-50">
-                    <tr>
-                        <th scope="col"
-                            class=" cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" wire:click="order('id')">
-                            ID
-                        </th>
-                        <th scope="col"
-                            class=" cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" wire:click="order('title')">
-                            Title
-                        </th>
-                        <th scope="col"
-                            class=" cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" wire:click="order('content')">
-                            Content
-                        </th>
-
-                        <th scope="col" class="relative px-6 py-3">
-                            <span class="sr-only">Edit</span>
-                        </th>
-                    </tr>
-                </thead>
-
-                <tbody class="bg-white divide-y divide-gray-200">
-
-                    @foreach ($posts as $posts)
+                    <thead class="bg-gray-50">
                         <tr>
-                            <td class="px-6 py-4">
-                                <div class="text-sm text-gray-900">
-                                    {{ $posts->id }}
-                                </div>
+                            <th scope="col"
+                                class=" cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                wire:click="order('id')">
+                                ID
+                                {{-- Sort --}}
+                                @if ($sort == 'id')
+                                    @if ($direction == 'asc')
+                                        <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    @else
+                                        <i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    @endif
 
-                            </td>
-                            <td class="px-6 py-4">
-                                <div class="text-sm text-gray-900">
-                                    {{ $posts->title }}
-                                </div>
+                                @else
+                                    <i class="fas fa-sort float-right mt-1"></i>
+                                @endif
 
-                            </td>
-                            <td class="px-6 py-4">
-                                <div class="text-sm text-gray-900">
-                                    {{ $posts->content }}
-                                </div>
+                            </th>
+                            <th scope="col"
+                                class=" cursor-pointer px-6 py-3  text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                wire:click="order('title')">
+                                Title
+                                {{-- Sort --}}
+                                @if ($sort == 'title')
+                                    @if ($direction == 'asc')
+                                        <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    @else
+                                        <i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    @endif
 
-                            </td>
-                            <td class="px-6 py-4">
-                                <div class="text-sm text-gray-900">
+                                @else
+                                    <i class="fas fa-sort float-right mt-1"></i>
+                                @endif
+                            </th>
+                            <th scope="col"
+                                class=" cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                                wire:click="order('content')">
+                                Content
+                                {{-- Sort --}}
+                                @if ($sort == 'content')
+                                    @if ($direction == 'asc')
+                                        <i class="fas fa-sort-alpha-up-alt float-right mt-1"></i>
+                                    @else
+                                        <i class="fas fa-sort-alpha-down-alt float-right mt-1"></i>
+                                    @endif
 
-                                </div>
+                                @else
+                                    <i class="fas fa-sort float-right mt-1"></i>
+                                @endif
+                            </th>
 
-                            </td>
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                            </th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table> 
+                    </thead>
+
+                    <tbody class="bg-white divide-y divide-gray-200">
+
+                        @foreach ($posts as $posts)
+                            <tr>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+                                        {{ $posts->id }}
+                                    </div>
+
+                                </td>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+                                        {{ $posts->title }}
+                                    </div>
+
+                                </td>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+                                        {{ $posts->content }}
+                                    </div>
+
+                                </td>
+                                <td class="px-6 py-4">
+                                    <div class="text-sm text-gray-900">
+
+                                    </div>
+
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             @else
                 <div class="px-6 py-4">
                     <p> No se encuentran coicidencias </p>
